e23f65232b6a67a4b3afd24b9326bae54e4203f8
Temm <samumert@gmail.com>
2024-10-17 14:37:02 +0200

    expand wildcard import

--- src/main/java/com/gtnh/findit/FindIt.java
+++ src/main/java/com/gtnh/findit/FindIt.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.gtnh.findit.IStackFilter.IStackFilterProvider;
-import com.gtnh.findit.handler.*;
+import com.gtnh.findit.handler.AdventureBackpackProvider;
+import com.gtnh.findit.handler.BackpackProvider;
+import com.gtnh.findit.handler.DraconicEvolutionProvider;
+import com.gtnh.findit.handler.ForestryStackFilterProvider;
+import com.gtnh.findit.handler.MinecraftProvider;
+import com.gtnh.findit.handler.ProjectRedExplorationProvider;
+import com.gtnh.findit.handler.ThaumcraftProvider;
 import com.gtnh.findit.service.blockfinder.BlockFindService;
 import com.gtnh.findit.service.blockfinder.ClientBlockFindService;
 import com.gtnh.findit.service.cooldown.SearchCooldownService;
